7e46fe8609e3f2de15ce6e3c6c911b9ebf3d6779
Randy Kropp <randy@Randys-MacBook-Pro.local>
2018-03-02 13:56:18 -0800

    updated main and renamed bno055 file

--- Minilab/main.py
+++ Minilab/main.py
@@ -3,13 +3,19 @@
 #
 
 import micropython
-import mma845x as i2c
+import bno055 as imu
+accel_range = b'00000000' ##accel = 2g
+accel_range = b'00000001' ##accel = 4g care if not in fusion mode. top 8 bits impact power and BW
 
 
 if __name__ == "__main__":
     '''Reads or Writes to i2c peripheral'''
     aye = pyb.I2C (1, pyb.I2C.MASTER)
-    adr_list = []
-    adr_list = pyb.I2C.scan (eye)
+    nerd = BNO055(aye, 0x28, accel_range)
+    x_data = nerd.get_ax_bits()
 
-
+    def scan_list()
+        aye = pyb.I2C (1, pyb.I2C.MASTER)
+        adr_list = []
+        adr_list = pyb.I2C.scan(aye)
+        return adr_list
